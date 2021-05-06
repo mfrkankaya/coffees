9c1953f1a8976c5c7f34e40de2c88c21d11f9e56
Furkan KAYA <furkan.kaya@deepensive.com>
2021-05-06 17:42:06 +0300

    update homepage

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import classes from '@styles/home.module.scss'
+import Searchbar from '@components/Searchbar'
+import CategoryList from 'src/containers/CategoryList'
+import CoffeeList from 'src/containers/CoffeeList'
 
 const Home = () => {
-  return <div>home</div>
+  return (
+    <div className={classes.layout}>
+      <div className={classes.drawer}>
+        <Searchbar />
+        <CategoryList />
+      </div>
+      <main className={classes.main}>
+        <CoffeeList />
+      </main>
+    </div>
+  )
 }
 
 export default Home
